c6c051f6427d1c0c281c2daa17d7ff3e99f46307
compactrex66 <compactrex66@gmail.com>
2025-02-19 08:48:30 +0100

    Custom enemy file

---
+++ addCustomEnemy/addCustomEnemy.php
@@ -0,0 +1,28 @@
+<?php
+    include "parsedown/Parsedown.php";
+    include "parsedown/ParsedownExtra.php";
+    include "dbConnection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/791dbbf45c.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <title>DnD App</title>
+</head>
+<body>
+    <header>Add custom enemy</header>
+    <main>
+        <form action="" method="post">
+            
+        </form>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/dist/markdown-it.min.js" integrity="sha256-OMcKHnypGrQOLZ5uYBKYUacX7Rx9Ssu91Bv5UDeRz2g=" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
